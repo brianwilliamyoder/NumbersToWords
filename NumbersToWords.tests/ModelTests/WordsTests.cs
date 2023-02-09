220eb9537ae97e34a289d8ca2f7edc333442aec9
Brian Yoder <brianyoder@gmail.com>
2023-02-08 20:48:29 -0800

    add returnsingledigit and returndoubledigit methods

--- NumbersToWords.tests/ModelTests/WordsTests.cs
+++ NumbersToWords.tests/ModelTests/WordsTests.cs
@@ -14,5 +14,34 @@ namespace NumbersToWords.Tests
       NumberWord new1 = new NumberWord();
       Assert.AreEqual(typeof(NumberWord), new1.GetType());
     }
-  }
-}
+
+    [TestMethod]
+    public void NumberDictionaries_OutputString_String()
+    {
+      NumberWord new1 = new NumberWord();
+      string zero = "zero";
+      string result = new1.singleDigit[0];
+      Assert.AreEqual(zero, result);
+    }
+
+    [TestMethod]
+    public void ReturnSingleDigitString_OutputString_String()
+    {
+      NumberWord new1 = new NumberWord();
+      string one = "one";
+      string result = new1.ReturnSingleDigitString(1);
+      Assert.AreEqual(one, result);
+
+    }
+   [TestMethod]
+    public void ReturnDoubleDigitString_OutputString_String()
+    {
+      NumberWord new1 = new NumberWord();
+      string ten = "ten";
+      string result = new1.ReturnDoubleDigitString(10);
+      Assert.AreEqual(ten, result);
+
+    }
+    
+   }
+ }
--- NumbersToWords/Models/Words.cs
+++ NumbersToWords/Models/Words.cs
@@ -1,25 +1,82 @@
 using System;
+using System.Collections.Generic;
 
 namespace NumbersToWords.Models
 {
   public class NumberWord
   {
-    public string Zero;
-    public string One;
-    public string Two;
-    // public string 3;
-    // public string 4;
-    // public string 5;
-    // public string 6;
-    // public string 7;
-    // public string 8;
-    // public string 9;
 
-    public NumberWord(int number)
+    public Dictionary<int, string> singleDigit = new Dictionary<int, string>
     {
-      
+      {0, "zero"},
+      {1, "one"},
+      {2, "two"},
+      {3, "three"},
+      {4, "four"},
+      {5, "five"},
+      {6, "six"},
+      {7, "seven"},
+      {8, "eight"},
+      {9, "nine"}
+    };
+
+    public Dictionary<int, string> teenDigitsEtc = new Dictionary<int, string>()
+    {
+      {11, "eleven"},
+      {12, "twelve"} ,
+      {13, "thirteen"},
+      {14, "fourteen"},
+      {15, "fifteen"},
+      {16, "sixteen"},
+      {17, "seventeen"},
+      {18, "eighteen"},
+      {19, "nineteen"}
+    };
+
+    public Dictionary<int, string> tYDigits = new Dictionary<int, string>()
+    {
+      {10, "ten"},
+      {20, "twenty"},
+      {30, "thirty"},
+      {40, "forty"},
+      {50, "fifty"},
+      {60, "sixty"},
+      {70, "seventy"},
+      {80, "eighty"},
+      {90, "ninety"}
+      };
+
+    public string ReturnSingleDigitString(int userNumber)
+    {
+      string output = this.singleDigit[userNumber];
+      return output;
     }
+
+    public string ReturnDoubleDigitString(int userNumber)
+    {
+
+      string stringNumber = userNumber.ToString();
+      char[] charNumber = stringNumber.ToCharArray();
+      if (stringNumber.Contains("0"))
+      {
+        return this.tYDigits[userNumber];
+
+      }
+      else if (stringNumber.Substring(0, 1) != "1")
+      {
+        return (this.tYDigits[userNumber]);
+      }
+      else
+      {
+        return this.teenDigitsEtc[userNumber];
+      }
+
+    }
+
+
+
+
   }
 }
 
-Dictionary<int, string> indexZero = new Dictionary<int, string>(){{0, "zero"}, {1,"one"}, {2,"two"}};
+
